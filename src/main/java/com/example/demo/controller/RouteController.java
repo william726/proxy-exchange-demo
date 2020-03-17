1bcbea2e627c49eea45dd1c39d8ba48914772d66
William Cheng <william726726@gmail.com>
2020-03-17 08:54:35 -0400

    Update RouteController.java

--- src/main/java/com/example/demo/controller/RouteController.java
+++ src/main/java/com/example/demo/controller/RouteController.java
@@ -1,45 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.cloud.gateway.webflux.ProxyExchange;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
 @RestController
 public class RouteController {
-    
     @RequestMapping(value="/**", method={ RequestMethod.GET, RequestMethod.POST })
-    public Mono<String> proxy(ServerHttpRequest request, ServerHttpResponse response, ProxyExchange<byte[]> proxy) throws Exception {
+    public Mono<ResponseEntity<byte[]>> proxy(ServerHttpRequest request, ServerHttpResponse response, ProxyExchange<byte[]> proxy) throws Exception {
         String path = proxy.path("/");
         
-        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024 * 10)).build();
-        
-        
         if (request.getMethodValue().startsWith("GET")) {
-            return WebClient
-                    .builder()
-                    .exchangeStrategies(exchangeStrategies)
-                    .baseUrl("https://stackoverflow.com/")
-                    .build()
-                    .get()
-                    .uri(path)
-                    .retrieve().bodyToMono(String.class);
+            return proxy.uri("https://stackoverflow.com/" + path).get();
         } else {
-            return WebClient
-                    .builder()
-                    .exchangeStrategies(exchangeStrategies)
-                    .baseUrl("https://stackoverflow.com/")
-                    .build()
-                    .post()
-                    .uri(path)
-                    .retrieve().bodyToMono(String.class);
+            return proxy.uri("https://stackoverflow.com/" + path).post();
         }
     }
 }
